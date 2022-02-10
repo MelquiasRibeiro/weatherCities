bffeacdc79066c553d7193ad965fc0cd971cd45e
Melquias Ribeiro <melquias10.mr@gmail.com>
2022-02-10 01:14:37 -0300

    feat: create a city card

--- src/components/SavedCityCard/index.tsx
+++ src/components/SavedCityCard/index.tsx
@@ -4,37 +4,43 @@ import {Margin} from '../Margin';
 import {Container, ColumContainer, RowContinaer} from './styles';
 import {Icon} from '../../assets/images/icons';
 import {useNavigation} from '@react-navigation/native';
+import {Weather} from '../../types/IWeather';
+interface ISavedCityCard {
+  city: Weather;
+}
 
-const SavedCityCard: React.FC = ({city}) => {
+const SavedCityCard: React.FC<ISavedCityCard> = ({city}) => {
   const {navigate} = useNavigation();
 
   const HandleNavigationToDetails = () => {
-    navigate('Details');
+    navigate('Details', {
+      city: city,
+    });
   };
   return (
     <Container onPress={HandleNavigationToDetails}>
       <RowContinaer>
         <ColumContainer>
           <Text weight="Regular" size={24} color="#000000">
-            {city.city}
+            {city.name}
           </Text>
           <Text weight="Regular" lineHeight={20} size={14} color="#000000">
-            {city.county}
+            Brasil{' '}
           </Text>
         </ColumContainer>
         <Text weight="Regular" size={34} color="#f28200">
-          {city.temp}
+          {city.main.temp}°
         </Text>
       </RowContinaer>
       <Margin margin={13} />
       <RowContinaer>
         <ColumContainer>
           <Text weight="Regular" lineHeight={20} size={14} color="#f28200">
-            {city.weater}
+            {city.weather[0].description}
           </Text>
           <Margin margin={2} />
           <Text weight="Regular" lineHeight={16} size={12} color="#000000">
-            {`${city.max} - ${city.min}`}
+            {`${city.main.temp_min}° - ${city.main.temp_max}°`}
           </Text>
         </ColumContainer>
         {city.favorite ? <Icon.Hearth /> : <Icon.HearthOutlined />}
